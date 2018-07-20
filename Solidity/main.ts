726f2505be57ade9d49ce6f42a7f73548a897d7d
Jongwhan Lee <leejw51@gmail.com>
2018-07-20 20:10:17 +0900

    OK

--- Solidity/main.ts
+++ Solidity/main.ts
@@ -1,8 +1,14 @@
-var Web3 = require('web3');
-var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-var Tx = require('ethereumjs-tx');
-var fs = require('fs');
+var Web3 = require('web3')
+var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+var Tx = require('ethereumjs-tx')
+var fs = require('fs')
 
-var accounts = web3.eth.accounts;
-console.log(accounts);
+async function run() {
+    var accounts = web3.eth.accounts
+    for (let m in accounts) {
+        var a = accounts[m]
+        console.log(`${a} ${web3.eth.getBalance(a)}`)
+    }
+}
 
+run()
