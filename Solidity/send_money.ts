f3009573ad5951bd624ce2aee1b270a535fa312d
Jongwhan Lee <leejw51@gmail.com>
2018-07-25 18:59:46 +0900

    show address

--- Solidity/send_money.ts
+++ Solidity/send_money.ts
@@ -1,12 +1,24 @@
 var Web3 = require('web3');
-var web3 = new Web3(new Web3.providers.HttpProvider('http://opo:8545'));
+var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 var Tx = require('ethereumjs-tx');
 var fs = require('fs');
 var config = require('../config.js');
 
 var keythereum = require("keythereum");
-run()
 
+
+
+async function getNonceUser(addr: string): Promise<any> {
+	var r = await web3.eth.getTransactionCount(addr)
+	var ret = web3.utils.toHex(r)
+	return ret
+}
+
+async function getGasPriceUser(): Promise<any> {
+	var r = await web3.eth.getGasPrice()
+	var ret = web3.utils.toHex(r)
+	return ret
+}
 
 function getPrivateKey(address: string) {
 	var datadir = "../data";
@@ -17,10 +29,15 @@ function getPrivateKey(address: string) {
 	return privateKey.toString('hex');
 }
 async function run() {
-	var key = getPrivateKey("0x9d16db949dffed9bf500eae1435a68a7cc9ec4df")
+	var from = "0x9d16db949dffed9bf500eae1435a68a7cc9ec4df"
+	var key = getPrivateKey(from)
 	console.log(key)
+	var nonce = await getNonceUser(from)
+	var gas = await getGasPriceUser()
+	console.log(`Nonce=${nonce}    Gas=${gas}`)
 }
 
 
+run()
 
 
