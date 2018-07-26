3b81c47ab09160964ffa7bc9768c7fef0d65cb5b
Jongwhan Lee <leejw51@gmail.com>
2018-07-26 15:22:47 +0900

    refactoring

--- Solidity/send_simple.ts
+++ Solidity/send_simple.ts
@@ -11,47 +11,6 @@ var Tx = require('ethereumjs-tx');
 var fs = require('fs');
 var config = require('../config.js');
 var keythereum = require("keythereum");
-async function getNonceUser(addr: string): Promise<any> {
-	var r = await web3.eth.getTransactionCount(addr)
-	return r
-}
-
-async function getGasPriceUser(): Promise<any> {
-	var ret = await web3.eth.getGasPrice()
-	var eth = web3.utils.fromWei(ret)
-	console.log(`gas price=${ret} wei   ${eth} eth`)
-	return ret
-}
-
-function getPrivateKey(address: string) {
-	var datadir = "../data";
-	const password = "";
-
-	var keyObject = keythereum.importFromFile(address, datadir);
-	var privateKey = keythereum.recover(password, keyObject);
-	return privateKey.toString('hex');
-}
-
-async function sendTransaction(signedTx: any) {
-	return new Promise(
-		(resolve) => {
-			web3.eth.sendSignedTransaction('0x' + signedTx.toString('hex'), function (
-				err: any, hash: any) {
-				var info: any = {}
-				info.Error = err
-				info.Hash = hash
-				resolve(info)
-			})
-		}
-	)
-}
-async function sendUserRawTransaction(rawTx: any, privateKey: any) {
-	const tx = new Tx(rawTx);
-	const privateKeyBuffer = privateKey;
-	tx.sign(privateKeyBuffer);
-	const signedTx = tx.serialize();
-	return sendTransaction(signedTx)
-}
 
 async function run() {
 	var from = "0x9d16db949dffed9bf500eae1435a68a7cc9ec4df"
